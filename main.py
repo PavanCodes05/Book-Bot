bdcde4de09b17e59a6b9368bbe6905139f3318c1
Pavan Prakash K <pavancodes05@gmail.com>
2025-01-13 17:32:59 +0530

    Refactor Code

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
-# Open the book
-with open("./books/frankenstein.txt", "r") as file:
-    file_contents = file.read()
+def read_book(book_path):
+    with open(book_path, "r") as book:
+        book_contents = book.read()
+    return book_contents
 
-# Analysis
-"""
-1. Count the number of words
-2. Count occurances of each characters
-"""
+def count_words(book_contents):
+    words = len(book_contents.split(" "))
+    return words
 
-words = len(file_contents.split(" "))
+def convert_lowercase(book_contents):
+    lowercase_contents = book_contents.strip().lower()
+    return lowercase_contents
 
-letters = dict()
+def count_letter(lowercase_contents):
+    letter_count = dict()
+    for letter in lowercase_contents:
+        letter_count[letter] = 1 + letter_count.get(letter, 0)
+    return letter_count
+    
+def generate_report(word_count, letter_count):
+    with open("report.txt", "w") as file:
+        file.write("____________________Beginning of the report_____________________\n\n")
 
-joined_words = file_contents.strip()
-lowercase = joined_words.lower()
+        file.write(f"Total Number of words: {word_count}\n\n")
 
-for letter in lowercase:
-    letters[letter] = 1 + letters.get(letter, 0)
+        for val, occ in letter_count.items():
+            file.write(f"The letter '{val}' has occured {str(letter_count[val])} times.\n")
 
-# Report
-with open("report.txt", "w") as file:
-    file.write("____________________Beginning of the report_____________________\n\n")
+        file.write("\n\n____________________End of the report___________________________")
 
-    file.write(f"Total Number of words: {words}\n\n")
+def main():
+    book_path = "./books/frankenstein.txt"
+    
+    book_contents = read_book(book_path)
+    
+    word_count = count_words(book_contents)
+    lowercase_contents = convert_lowercase(book_contents)
 
-    for val, occ in letters.items():
-        file.write(f"The letter '{val}' has occured {str(letters[val])} times.\n")
+    letter_count = count_letter(lowercase_contents)
+    
+    generate_report(word_count, letter_count)
 
-    file.write("\n\n____________________End of the report___________________________")
+main()
