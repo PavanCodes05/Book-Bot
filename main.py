98a6ded0697ab4080f3836a1815c3f2c7e40cb9d
PavanCodes05 <pavancodes05@gmail.com>
2025-01-16 22:17:56 +0530

    Remove Comments

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-# Reads the book content and returns a array of words
 def read_book(book_path):
     with open(book_path, "r") as book:
         book_contents = book.read()
     return book_contents
 
-# Reads the book content and return the number of words
 def count_words(book_contents):
     words = len(book_contents.split(" "))
     return words
 
-# Reads the book content and converts uppercase to lowercase 
 def convert_lowercase(book_contents):
     lowercase_contents = book_contents.strip().lower()
     return lowercase_contents
 
-# Reads the lowercase contents and returns the dictionary with each letter occurances
 def count_letter(lowercase_contents):
     letter_count = dict()
     for letter in lowercase_contents:
         letter_count[letter] = 1 + letter_count.get(letter, 0)
     return letter_count
 
-# Generates Report.txt file 
 def generate_report(word_count, letter_count):
     with open("report.txt", "w") as file:
         file.write("____________________Beginning of the report_____________________\n\n")
@@ -33,7 +28,6 @@ def generate_report(word_count, letter_count):
 
         file.write("\n\n____________________End of the report___________________________")
 
-# Main function that organizes all of the code (Functional Programming)  
 def main():
     book_path = "./books/frankenstein.txt"
     
